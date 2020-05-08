6dff16f3b0413bafc2c5e24a3a4886677abb7f11
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 18:46:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,21 @@ def change
    create_table :costume_stores do |t|
      t.string :name
      t.string :location
+     t.string  :costume_inventory
+     t.sting   :num_of_employees
+
    end
 end
 
 
 end
+
+
+
+name
+location
+number of costumes, or "costume inventory"
+number of employees
+whether or not it's still in business
+opening time
+closing time
