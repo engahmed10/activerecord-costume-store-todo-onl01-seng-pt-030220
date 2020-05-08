b597b411c722925a3c7b51f7164e2a00dd5ab0ae
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 18:18:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
         create_table :costumes do |t|
           t.string :name
           t.integer :price
-          t.string  :image
+          t.string  :url
           t.integer :size
         end
 
