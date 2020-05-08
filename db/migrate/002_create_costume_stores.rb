d09eb5cb1e991814c49d3eec2a238489411a25b8
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 18:50:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ def change
      t.string :name
      t.string :location
      t.string :costume_inventory
-     t.string :num_of_employees
+     t.integer :num_of_employees
      t.boolean :still_in_business
      t.datetime :opening_time
      t.datetime :closing_time
