48e3a92aea7f8c016e89c6dc0071d319727a6b8d
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 18:36:54 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,10 +1,9 @@
 # Create your costume_stores migration here
 
-class CostumeStore  < ActiveRecord::Migration[5.2]
+class CreateCostumeStores  < ActiveRecord::Migration[5.2]
 
 def change
    create_table :costumestores do |t|
-
      t.sting :name
    end
 end
