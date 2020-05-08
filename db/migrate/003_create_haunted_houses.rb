869e459d67070cb103df84ca4b9b905580a4b615
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-08 19:05:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHuntedHouses < ActiveRecord::Migration[5.2]
      create_table :haunted_houses do |t|
        t.string :name
        t.string :location
-       t.string :costume_inventory
+       t.string :theme
        t.integer :num_of_employees
        t.boolean :still_in_business
        t.datetime :opening_time
